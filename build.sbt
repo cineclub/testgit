79e86c67894977a7b529d07af3a76e77fec5e0fa
Kim Je Min <cineclub.kjm@gmail.com>
2013-12-14 15:52:16 +0900

    first commit

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "testgit"
+
+version := "1.0"
+
+scalaVersion := "2.10.2"
---
+++ src/main/scala/Test.scala
@@ -0,0 +1,11 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: cineclub
+ * Date: 12/14/13
+ * Time: 3:47 PM
+ * To change this template use File | Settings | File Templates.
+ */
+object Test extends App{
+  println("hello")
+
+}
